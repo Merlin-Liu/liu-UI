b9177a01b8c0b0cae8af504fe8bd2648aecf13c6
liuguangfu <is_liuguangfu@163.com>
2019-08-10 18:18:55 +0800

    removeResizeListener fn

--- src/utils/resize-event.ts
+++ src/utils/resize-event.ts
@@ -21,3 +21,11 @@ export const addResizeListener = function(element: ObserverElement, fn: Function
 
   element.__resizeListeners.push(fn);
 }
+
+export const removeResizeListener = function(element: ObserverElement, fn: Function): void {
+  if (!element || !element.__resizeListeners) return
+
+  element.__resizeListeners.splice(element.__resizeListeners.indexOf(fn), 1)
+
+  if (!element.__resizeListeners) element.__resizeObsever.disconnect()
+}
