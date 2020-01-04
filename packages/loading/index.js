292748981cf2f932ed159175c870d1d3357f496a
liuguangfu <is_liuguangfu@163.com>
2020-01-04 15:45:11 +0800

    loading组件优化

--- packages/loading/index.js
+++ packages/loading/index.js
@@ -1,16 +1,24 @@
 import Vue from 'vue';
 import loadingVue from './loading.vue'
 
+let loadingInstance;
+
 const LoadingConstructor = Vue.extend(loadingVue);
 
 LoadingConstructor.prototype.close = function() {
+  loadingInstance = undefined
+
   if (this.$el && this.$el.parentNode) {
-    this.$el.parentNode.removeChild(this.$el);
+    this.$el.parentNode.removeChild(this.$el)
   }
-  this.$destroy();
+  this.$destroy()
 }
 
 const Loading = () => {
+  if (loadingInstance) {
+    return loadingInstance
+  }
+
   const parent = document.body
 
   const instance = new LoadingConstructor({
@@ -19,6 +27,7 @@ const Loading = () => {
 
   parent.appendChild(instance.$el)
 
+  loadingInstance = instance
   return instance
 }
 
